0496bc17fe7b73a367f62404a263bca6eda0a3e6
Jon Staff <jstaff@indatus.com>
2013-10-08 16:25:09 -0400

    Progress on the sample application

--- samples/src/main/java/com/indatus/smoothseekbar/samples/MainActivity.java
+++ samples/src/main/java/com/indatus/smoothseekbar/samples/MainActivity.java
@@ -1,16 +1,23 @@
 package com.indatus.smoothseekbar.samples;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 
 import com.indatus.smoothseekbar.library.SmoothSeekBar;
 
-public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 	public static final String TAG = MainActivity.class.getSimpleName();
 
 	SmoothSeekBar mSeekBar;
+	MediaPlayer mPlayer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,14 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
 
 		mSeekBar = (SmoothSeekBar) findViewById(R.id.seekbar);
 		mSeekBar.setOnSeekBarChangeListener(this);
+
+		Button btnPlay = (Button) findViewById(R.id.btn_play);
+		btnPlay.setOnClickListener(this);
+
+		Button btnStop = (Button) findViewById(R.id.btn_stop);
+		btnStop.setOnClickListener(this);
+
+		mPlayer = new MediaPlayer();
 	}
 
 	@Override
@@ -26,6 +41,28 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private void playAudio() {
+		try {
+			mPlayer.reset();
+			mPlayer.setDataSource("somewhere");
+			mPlayer.prepare();
+			mPlayer.start();
+
+			mSeekBar.setProgress(0);
+			mSeekBar.setEndTime(mPlayer.getDuration());
+			mSeekBar.beginAnimating();
+		}
+		catch (IOException e) {
+			Log.w(TAG, "The audio file you're trying to play isn't available.");
+			e.printStackTrace();
+		}
+	}
+
+	private void stopAudio() {
+		mPlayer.stop();
+		mSeekBar.endAnimation();
 	}
 
 	//     ___       _             __                  ___                 _                           _        _   _
@@ -36,17 +73,31 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
 	//                                                              |_|
 
 	@Override
-	public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
 	}
 
 	@Override
 	public void onStartTrackingTouch(SeekBar seekBar) {
-        mSeekBar.pauseAnimating();
+		mSeekBar.pauseAnimating();
 	}
 
 	@Override
 	public void onStopTrackingTouch(SeekBar seekBar) {
-        mSeekBar.beginAnimating();
+		mSeekBar.beginAnimating();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.btn_play:
+				// do stuff
+				playAudio();
+				break;
+			case R.id.btn_stop:
+				// do more stuff
+                stopAudio();
+				break;
+		}
 	}
 }
