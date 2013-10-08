70cc27ff40742fbe89a5292ecb8e1b518874e815
Jon Staff <jstaff@indatus.com>
2013-10-08 16:07:57 -0400

    Implementing a sample project

--- samples/src/main/java/com/indatus/smoothseekbar/samples/MainActivity.java
+++ samples/src/main/java/com/indatus/smoothseekbar/samples/MainActivity.java
@@ -3,14 +3,22 @@ package com.indatus.smoothseekbar.samples;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.SeekBar;
 
-public class MainActivity extends Activity {
+import com.indatus.smoothseekbar.library.SmoothSeekBar;
+
+public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+	public static final String TAG = MainActivity.class.getSimpleName();
+
+	SmoothSeekBar mSeekBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mSeekBar = (SmoothSeekBar) findViewById(R.id.seekbar);
+		mSeekBar.setOnSeekBarChangeListener(this);
 	}
 
 	@Override
@@ -20,4 +28,25 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	//     ___       _             __                  ___                 _                           _        _   _
+	//    |_ _|_ __ | |_ ___ _ __ / _| __ _  ___ ___  |_ _|_ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _| |_(_) ___  _ __  ___
+	//     | || '_ \| __/ _ \ '__| |_ / _` |/ __/ _ \  | || '_ ` _ \| '_ \| |/ _ \ '_ ` _ \ / _ \ '_ \| __/ _` | __| |/ _ \| '_ \/ __|
+	//     | || | | | ||  __/ |  |  _| (_| | (_|  __/  | || | | | | | |_) | |  __/ | | | | |  __/ | | | || (_| | |_| | (_) | | | \__ \
+	//    |___|_| |_|\__\___|_|  |_|  \__,_|\___\___| |___|_| |_| |_| .__/|_|\___|_| |_| |_|\___|_| |_|\__\__,_|\__|_|\___/|_| |_|___/
+	//                                                              |_|
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+        mSeekBar.pauseAnimating();
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+        mSeekBar.beginAnimating();
+	}
 }
